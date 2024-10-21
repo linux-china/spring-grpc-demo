4764a9ade9cb454a47d79d78ad1e5c01b882ce16
Libing Chen <libing.chen@gmail.com>
2024-10-21 20:28:16 +0800

    test: SpringGrpcClientApp

---
+++ src/test/java/org/springframework/grpc/sample/client/SpringGrpcClientApp.java
@@ -0,0 +1,11 @@
+package org.springframework.grpc.sample.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGrpcClientApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGrpcClientApp.class, args);
+    }
+}
