2fd93aca8a9f3f4d2e8a82eb3599ee3f99e4f052
Libing Chen <libing.chen@gmail.com>
2024-10-26 10:12:02 +0800

    app: SimpleServiceImpl

---
+++ src/main/java/org/springframework/grpc/sample/SimpleServiceImpl.java
@@ -0,0 +1,43 @@
+package org.springframework.grpc.sample;
+
+import io.grpc.stub.StreamObserver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.grpc.sample.proto.HelloReply;
+import org.springframework.grpc.sample.proto.HelloRequest;
+import org.springframework.grpc.sample.proto.SimpleGrpc;
+import org.springframework.scheduling.annotation.Async;
+
+//@Service
+public class SimpleServiceImpl extends SimpleGrpc.SimpleImplBase {
+
+    private static Logger log = LoggerFactory.getLogger(SimpleServiceImpl.class);
+
+    @Override
+    public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
+        log.info("Hello " + req.getName());
+        HelloReply reply = HelloReply.newBuilder().setMessage("Hello! " + req.getName()).build();
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    @Async
+    public void streamHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
+        log.info("Hello " + req.getName());
+        int count = 0;
+        while (count < 10) {
+            HelloReply reply = HelloReply.newBuilder().setMessage("Hello(" + count + ") ==> " + req.getName()).build();
+            responseObserver.onNext(reply);
+            count++;
+            try {
+                Thread.sleep(1000L);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                responseObserver.onError(e);
+                return;
+            }
+        }
+        responseObserver.onCompleted();
+    }
+}
