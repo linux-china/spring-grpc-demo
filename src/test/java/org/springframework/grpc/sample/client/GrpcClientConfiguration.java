8ee5a83403739551557f3757691af4ff58b71332
Libing Chen <libing.chen@gmail.com>
2025-01-15 23:53:24 +0800

    chore: polishing

--- src/test/java/org/springframework/grpc/sample/client/GrpcClientConfiguration.java
+++ src/test/java/org/springframework/grpc/sample/client/GrpcClientConfiguration.java
@@ -1,6 +1,5 @@
 package org.springframework.grpc.sample.client;
 
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.grpc.client.GrpcChannelFactory;
@@ -11,6 +10,6 @@ import org.springframework.grpc.sample.proto.SimpleGrpc;
 public class GrpcClientConfiguration {
     @Bean
     SimpleGrpc.SimpleBlockingStub stub(GrpcChannelFactory channels) {
-        return SimpleGrpc.newBlockingStub(channels.createChannel("local").build());
+        return SimpleGrpc.newBlockingStub(channels.createChannel("local"));
     }
 }
